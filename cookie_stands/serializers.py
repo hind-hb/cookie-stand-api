3ed3ff35a63555c3746d938e6e3f935b8cb3b689
hind-hb <hind.hbashenh@gmail.com>
2022-06-01 01:44:01 +0300

    edit

--- cookie_stands/serializers.py
+++ cookie_stands/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import cookie_stands
+from cookie_stands.models import cookie_stands
 
 
 class cookie_standsSerializer(serializers.ModelSerializer):
--- cookie_stands/urls.py
+++ cookie_stands/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import cookie_standsList, cookie_standsDetail
+from cookie_stands.views import cookie_standsList, cookie_standsDetail
 
 urlpatterns = [
     path("", cookie_standsList.as_view(), name="thing_list"),
